5624708bd39f5e8999c449195193858d34cd9d40
joshua <joshua@joshuagranick.com>
2011-08-23 21:35:50 +0000

    Fixed the ObjectHash iterator() for the Haxe Flash target, and ported FilterActuator, MethodActuator and TransformActuator over

--- Source/haXe/com/eclecticdesignstudio/motion/Actuate.hx
+++ Source/haXe/com/eclecticdesignstudio/motion/Actuate.hx
@@ -1,18 +1,17 @@
 ﻿/**
  * @author Joshua Granick
- * @version 1.25
+ * @version 1.31
  */
 
 
 package com.eclecticdesignstudio.motion;
 
 
+import com.eclecticdesignstudio.motion.actuators.FilterActuator;
 import com.eclecticdesignstudio.motion.actuators.GenericActuator;
-//import com.eclecticdesignstudio.motion.actuators.MethodActuator;
-//import com.eclecticdesignstudio.motion.actuators.MotionInternal;
-//import com.eclecticdesignstudio.motion.actuators.MotionPathActuator;
+import com.eclecticdesignstudio.motion.actuators.MethodActuator;
 import com.eclecticdesignstudio.motion.actuators.SimpleActuator;
-//import com.eclecticdesignstudio.motion.actuators.TransformActuator;
+import com.eclecticdesignstudio.motion.actuators.TransformActuator;
 import com.eclecticdesignstudio.motion.easing.Expo;
 import com.eclecticdesignstudio.motion.easing.IEasing;
 import flash.display.DisplayObject;
@@ -39,29 +38,49 @@ class Actuate {
 		
 		stop (target, properties);
 		
-		#if cpp
+		if (customActuator == null) {
 			
-			// Type.createInstance doesn't work right now for the properties object
+			customActuator = defaultActuator;
 			
-			var actuator:GenericActuator = new SimpleActuator (target, 0, properties);
-			
-		#else
-			
-			var actuateClass:Class <GenericActuator> = customActuator;
-			
-			if (actuateClass == null) {
-				
-				actuateClass = defaultActuator;
-				
-			}
-			
-			var actuator:GenericActuator = Type.createInstance (actuateClass, [ target, 0, properties ] );
-			
-		#end
+		}
 		
+		var actuator:GenericActuator = createInstance (customActuator, target, 0, properties);
 		actuator.apply ();
 		
 		return actuator;
+		
+	}
+	
+	
+	private static function createInstance (actuator:Class <GenericActuator>, target:Dynamic, duration:Float, properties:Dynamic):GenericActuator {
+		
+		// Type.createInstance isn't working properly on CPP, so have to create manually
+		
+		if (actuator == SimpleActuator) {
+			
+			return new SimpleActuator (target, duration, properties);
+			
+		}
+		
+		if (actuator == FilterActuator) {
+			
+			return new FilterActuator (target, duration, properties);
+			
+		}
+		
+		if (actuator == TransformActuator) {
+			
+			return new TransformActuator (target, duration, properties);
+			
+		}
+		
+		if (actuator == MethodActuator) {
+			
+			return new MethodActuator (target, duration, properties);
+			
+		}
+		
+		return null;
 		
 	}
 	
@@ -115,10 +134,7 @@ class Actuate {
 	//public static function pause (... targets:Array):void {
 	public static function pause (target:Dynamic):Void {
 		
-		var actuator:GenericActuator;
-		var library:Array <GenericActuator>;
-		
-		library = getLibrary (target);
+		var library:Array <GenericActuator> = getLibrary (target);
 		
 		for (actuator in library) {
 			
@@ -133,9 +149,8 @@ class Actuate {
 		
 		for (library in targetLibraries) {
 			
-			for (i in 0...library.length) {
+			for (actuator in library) {
 				
-				var actuator:GenericActuator = library[i];
 				actuator.pause ();
 				
 			}
@@ -149,8 +164,6 @@ class Actuate {
 	 * Resets Actuate by stopping and removing tweens for all objects
 	 */
 	public static function reset ():Void {
-		
-		var actuator:GenericActuator;
 		
 		for (library in targetLibraries) {
 			
@@ -173,10 +186,7 @@ class Actuate {
 	 */
 	public static function resume (target:Dynamic):Void {
 		
-		var actuator:GenericActuator;
-		var library:Array <GenericActuator>;
-		
-		library = getLibrary (target);
+		var library:Array <GenericActuator> = getLibrary (target);
 		
 		for (actuator in library) {
 			
@@ -213,18 +223,17 @@ class Actuate {
 		
 		if (target != null) {
 			
-			var actuator:GenericActuator;
 			var library:Array <GenericActuator> = getLibrary (target);
-			
-			var temp = {};
 			
 			if (Std.is (properties, String)) {
 				
+				var temp = {};
 				Reflect.setField (temp, properties, null);
-				//temp[properties] = null;
 				properties = temp;
 				
 			} else if (Std.is (properties, Array)) {
+				
+				var temp = {};
 				
 				for (i in Reflect.fields (properties)) {
 					
@@ -292,18 +301,29 @@ class Actuate {
 			
 			if (duration > 0) {
 				
-				// Type.createInstance doesn't work right now for the properties object
-				var actuator:GenericActuator = new SimpleActuator (target, duration, properties);
-				
-				/*var actuateClass:Class <GenericActuator> = customActuator;
-				
-				if (actuateClass == null) {
+				if (customActuator == null) {
 					
-					actuateClass = defaultActuator;
+					customActuator = defaultActuator;
 					
 				}
 				
-				var actuator:GenericActuator = Type.createInstance (actuateClass, [ target, duration, properties ] );*/
+				var actuator:GenericActuator = createInstance (customActuator, target, duration, properties);
+				var library:Array <GenericActuator> = getLibrary (actuator.target);
+				
+				if (overwrite) {
+					
+					for (childActuator in library) {
+						
+						childActuator.stop (actuator.properties, false, false);
+						
+					}
+					
+				}
+				
+				library.push (actuator);
+				actuator.move ();
+				
+				/*var actuator:GenericActuator = createInstance (customActuator, target, duration, properties);
 				
 				if (overwrite) {
 					
@@ -312,26 +332,9 @@ class Actuate {
 				}
 				
 				var library:Array <GenericActuator> = getLibrary (target);
-				
-				/*if (overwrite) {
-					
-					for (childActuator in library) {
-						
-						var childActuatorInternal:MotionInternal = childActuator;
-						
-						if (childActuatorInternal.target == target) {
-							
-							childActuatorInternal.stop (properties, false, false);
-							
-						}
-						
-					}
-					
-				}*/
-				
 				library.push (actuator);
 				
-				actuator.move ();
+				actuator.move ();*/
 				
 				return actuator;
 				
@@ -377,21 +380,21 @@ class Actuate {
 	 * @param	overwrite		Sets whether previous tweens for the same target and properties will be overwritten (Default is true)
 	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
 	 */
-	//public static function update (target:Function, duration:Number, start:Array = null, end:Array = null, overwrite:Boolean = true):GenericActuator {
+	public static function update (target:Dynamic, duration:Float, start:Array <Dynamic> = null, end:Array <Dynamic> = null, overwrite:Bool = true):IGenericActuator {
 		
-		//var properties:Object = { start: start, end: end };
+		var properties:Dynamic = { start: start, end: end };
 		
-		//return tween (target, duration, properties, overwrite, MethodActuator);
+		return tween (target, duration, properties, overwrite, MethodActuator);
 		
-	//}
+	}
 	
 	
 }
 
 
-//import com.eclecticdesignstudio.motion.actuators.FilterActuator;
+import com.eclecticdesignstudio.motion.actuators.FilterActuator;
 import com.eclecticdesignstudio.motion.actuators.GenericActuator;
-//import com.eclecticdesignstudio.motion.actuators.TransformActuator;
+import com.eclecticdesignstudio.motion.actuators.TransformActuator;
 import com.eclecticdesignstudio.motion.Actuate;
 import flash.display.DisplayObject;
 import flash.filters.BitmapFilter;
@@ -402,33 +405,33 @@ import flash.geom.Matrix;
 private class EffectsOptions {
 
 
-private var duration:Float;
-private var overwrite:Bool;
-private var target:DisplayObject;
+	private var duration:Float;
+	private var overwrite:Bool;
+	private var target:DisplayObject;
 
 
-public function new (target:DisplayObject, duration:Float, overwrite:Bool) {
-	
-	this.target = target;
-	this.duration = duration;
-	this.overwrite = overwrite;
-	
-}
+	public function new (target:DisplayObject, duration:Float, overwrite:Bool) {
+		
+		this.target = target;
+		this.duration = duration;
+		this.overwrite = overwrite;
+		
+	}
 
 
-/**
- * Creates a new BitmapFilter tween
- * @param	reference		A reference to the target's filter, which can be an array index or the class of the filter
- * @param	properties		The end properties to use for the tween
- * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
- */
-/*public function filter (reference:Dynamic, properties:Dynamic):GenericActuator {
-	
-	properties.filter = reference;
-	
-	return Actuate.tween (target, duration, properties, overwrite, FilterActuator);
-	
-}*/
+	/**
+	 * Creates a new BitmapFilter tween
+	 * @param	reference		A reference to the target's filter, which can be an array index or the class of the filter
+	 * @param	properties		The end properties to use for the tween
+	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
+	 */
+	public function filter (reference:Dynamic, properties:Dynamic):IGenericActuator {
+		
+		properties.filter = reference;
+		
+		return Actuate.tween (target, duration, properties, overwrite, FilterActuator);
+		
+	}
 
 
 }
@@ -437,67 +440,67 @@ public function new (target:DisplayObject, duration:Float, overwrite:Bool) {
 private class TransformOptions {
 
 
-private var duration:Float;
-private var overwrite:Bool;
-private var target:Dynamic;
+	private var duration:Float;
+	private var overwrite:Bool;
+	private var target:Dynamic;
 
 
-public function new (target:Dynamic, duration:Float, overwrite:Bool) {
-	
-	this.target = target;
-	this.duration = duration;
-	this.overwrite = overwrite;
-	
-}
-
-
-/**
- * Creates a new ColorTransform tween
- * @param	color		The color value
- * @param	strength		The percentage amount of tint to apply (Default is 1)
- * @param	alpha		The end alpha of the target. If you wish to tween alpha and tint simultaneously, you must do them both as part of the ColorTransform. A value of null will make no change to the alpha of the object (Default is null)
- * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
- */
-/*public function color (value:Float = 0x000000, strength:Float = 1, alpha:Dynamic = null):GenericActuator {
-	
-	var properties:Object = { colorValue: value, colorStrength: strength };
-	
-	if (Type.typeof (alpha) == Float) {
+	public function new (target:Dynamic, duration:Float, overwrite:Bool) {
 		
-		properties.colorAlpha = alpha;
+		this.target = target;
+		this.duration = duration;
+		this.overwrite = overwrite;
 		
 	}
-	
-	return Actuate.tween (target, duration, properties, overwrite, TransformActuator);
-	
-}*/
 
 
-/**
- * Creates a new SoundTransform tween
- * @param	volume		The end volume for the target, or null if you would like to ignore this property (Default is null)
- * @param	pan		The end pan for the target, or null if you would like to ignore this property (Default is null)
- * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
- */
-/*public function sound (volume:Dynamic = null, pan:Dynamic = null):GenericActuator {
-	
-	var properties:Object = new Object ();
-	
-	if (Type.typeof (volume) == Float) {
+	/**
+	 * Creates a new ColorTransform tween
+	 * @param	color		The color value
+	 * @param	strength		The percentage amount of tint to apply (Default is 1)
+	 * @param	alpha		The end alpha of the target. If you wish to tween alpha and tint simultaneously, you must do them both as part of the ColorTransform. A value of null will make no change to the alpha of the object (Default is null)
+	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
+	 */
+	public function color (value:Float = 0x000000, strength:Float = 1, alpha:Null <Float>):IGenericActuator {
 		
-		properties.soundVolume = volume;
+		var properties:Dynamic = { colorValue: value, colorStrength: strength };
+		
+		if (alpha != null) {
+			
+			properties.colorAlpha = alpha;
+			
+		}
+		
+		return Actuate.tween (target, duration, properties, overwrite, TransformActuator);
 		
 	}
-	
-	if (Type.typeof (pan) == Float) {
+
+
+	/**
+	 * Creates a new SoundTransform tween
+	 * @param	volume		The end volume for the target, or null if you would like to ignore this property (Default is null)
+	 * @param	pan		The end pan for the target, or null if you would like to ignore this property (Default is null)
+	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
+	 */
+	public function sound (volume:Null <Float>, pan:Null <Float>):IGenericActuator {
 		
-		properties.soundPan = pan;
+		var properties:Dynamic = {};
+		
+		if (volume != null) {
+			
+			properties.soundVolume = volume;
+			
+		}
+		
+		if (pan != null) {
+			
+			properties.soundPan = pan;
+			
+		}
+		
+		return Actuate.tween (target, duration, properties, overwrite, TransformActuator);
 		
 	}
-	
-	return Actuate.tween (target, duration, properties, overwrite, TransformActuator);
-	
-}*/
 
 
 }
@@ -520,7 +523,7 @@ private class TweenTimer {
 
 
 #if flash
-import flash.utils.Dictionary;
+import flash.utils.TypedDictionary;
 #end
 
 
@@ -529,7 +532,7 @@ private class ObjectHash <T> {
 	
 	#if flash
 	
-	private var dictionary:Dictionary;
+	private var dictionary:TypedDictionary <Dynamic, T>;
 	
 	#else
 	
@@ -542,7 +545,7 @@ private class ObjectHash <T> {
 		
 		#if flash
 		
-		dictionary = new Dictionary (true);
+		dictionary = new TypedDictionary <Dynamic, T> ();
 		
 		#else
 		
@@ -557,7 +560,7 @@ private class ObjectHash <T> {
 		
 		#if flash
 		
-		return untyped dictionary[key] != null;
+		return dictionary.exists (key);
 		
 		#else
 		
@@ -572,7 +575,7 @@ private class ObjectHash <T> {
 		
 		#if flash
 		
-		return untyped dictionary[key];
+		return dictionary.get (key);
 		
 		#else
 		
@@ -602,7 +605,7 @@ private class ObjectHash <T> {
 		
 		#if flash
 		
-		return untyped __keys__(dictionary);
+		return dictionary.iterator ();
 		
 		#else
 		
@@ -617,7 +620,7 @@ private class ObjectHash <T> {
 		
 		#if flash
 		
-		untyped dictionary[key] = null;
+		dictionary.delete (key);
 		
 		#else
 		
@@ -632,7 +635,7 @@ private class ObjectHash <T> {
 		
 		#if flash
 		
-		untyped dictionary[key] = value;
+		dictionary.set (key, value);
 		
 		#else
 		
---
+++ Source/haXe/com/eclecticdesignstudio/motion/actuators/MethodActuator.hx
@@ -0,0 +1,87 @@
+﻿package com.eclecticdesignstudio.motion.actuators;
+
+
+/**
+ * @author Joshua Granick
+ * @version 1.2
+ */
+class MethodActuator extends SimpleActuator {
+	
+	
+	private var tweenProperties:Dynamic;
+	
+	
+	public function new (target:Dynamic, duration:Float, properties:Dynamic) {
+		
+		tweenProperties = { };
+		
+		super (target, duration, properties);
+		
+		if (!properties.start) {
+			
+			properties.start = new Array <Dynamic> ();
+			
+		}
+		
+		if (!properties.end) {
+			
+			properties.end = properties.start;
+			
+		}
+		
+	}
+	
+	
+	public override function apply ():Void {
+		
+		Reflect.callMethod (null, target, properties.end);
+		
+	}
+	
+	
+	private override function initialize ():Void {
+		
+		var details:PropertyDetails;
+		var propertyName:String;
+		var start:Dynamic;
+		
+		for (i in 0...properties.start.length) {
+			
+			propertyName = "param" + i;
+			start = properties.start[i];
+			
+			Reflect.setField (tweenProperties, propertyName, start);
+			
+			if (Std.is (start, Float) || Std.is (start, Int)) {
+				
+				details = new PropertyDetails (tweenProperties, propertyName, start, properties.end[i] - start);
+				propertyDetails.push (details);
+				
+			}
+			
+		}
+		
+		detailsLength = propertyDetails.length;
+		initialized = true;
+		
+	}
+	
+	
+	private override function update (currentTime:Float):Void {
+		
+		super.update (currentTime);
+		
+		var parameters:Array <Dynamic> = new Array <Dynamic> ();
+		
+		for (i in 0...properties.start.length) {
+			
+			parameters.push (Reflect.field (tweenProperties, "param" + i));
+			
+		}
+		
+		Reflect.callMethod (null, target, parameters);
+		
+	}
+	
+	
+}
